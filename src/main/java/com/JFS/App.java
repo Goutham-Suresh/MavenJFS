0228836a435154b4dbb93010d8a5bb4ba2a1acf2
Goutham-Suresh <gouthams2000@gmail.com>
2023-08-16 13:19:21 +0530

    First Commit

---
+++ src/main/java/com/JFS/App.java
@@ -0,0 +1,13 @@
+package com.JFS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
